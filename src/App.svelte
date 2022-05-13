c82be77b1f61cabcb53940511b2621ef17065420
faridulhassan <faridul940@gmail.com>
2022-05-14 03:00:36 +0600

    Launchpads and it's details page api data loaded and showed. Styles will be added later

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,8 @@
     import LaunchDetails from "./routes/LaunchDetails.svelte";
     import Launches from "./routes/Launches.svelte";
     import Launchpads from "./routes/Launchpads.svelte";
+    import LaunchpadDetails from "./routes/LaunchpadDetails.svelte";
+
     export let url = "";
 </script>
 
@@ -14,6 +16,7 @@
         <Nav />
         <Router>
             <Route path="launchpads" component={Launchpads} />
+            <Route path="launchpads/:id" component={LaunchpadDetails} />
             <Route path="launches" component={Launches} />
             <Route path="launches/:id" component={LaunchDetails} />
             <Route path="/" component={CompanyInfo} />
