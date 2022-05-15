f8e6e6d7b7314ae890c060c9ececfc8029a06dde
faridulhassan <faridul940@gmail.com>
2022-05-15 13:04:31 +0600

    rendered LaunchCard lists

--- src/routes/Launches.svelte
+++ src/routes/Launches.svelte
@@ -1,32 +1,56 @@
 <script>
     import { onMount } from "svelte";
-    import { Link } from "svelte-routing";
+
+    import LaunchCard from "../components/LaunchCard.svelte";
+    import Loader from "../components/Loader.svelte";
 
     import { getData } from "../utils";
 
-    let launches = [];
+    let launches;
 
+    let isLoading = false;
+    function setLoading(is_loading) {
+        isLoading = is_loading;
+    }
+    let error = null;
     onMount(() => {
+        setLoading(true);
         getData({
             path: "/launches"
         })
             .then((result) => {
+                setLoading(false);
                 launches = result;
-                console.log(launches[0]);
+                console.log(launches);
             })
-            .catch((error) => {
-                console.error(error);
+            .catch((err) => {
+                setLoading(false);
+                if (err.status === 404) {
+                    error = "Sorry, requested data not found! Please try again later.";
+                } else if (err.data) {
+                    error = err.data;
+                } else {
+                    error = "Something went wrong! Please try again later.";
+                }
             });
     });
 </script>
 
+<svelte:head>
+    <title>Launches</title>
+</svelte:head>
 <div>
-    <h1>Launch Page</h1>
-    <ul class="launches-list">
-        {#each launches as launch (launch.id)}
-            <li>
-                <Link to={`/launches/${launch.id}`} state={launch}>{launch.name}</Link>
-            </li>
-        {/each}
-    </ul>
+    <h1 class="text-4xl mb-3">Launches</h1>
+    {#if isLoading}
+        <Loader />
+    {/if}
+    {#if launches}
+        <div class="launches-list grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+            {#each launches as launch (launch.id)}
+                <LaunchCard {launch} />
+            {/each}
+        </div>
+    {:else if error}
+        <p class="mt-64 text-red-500 font-bold text-center text-3xl sm:text-5xl">{error}</p>
+    {/if}
 </div>
