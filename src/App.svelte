56d8f37b1ab3adb249b21f4d10ac50268f687832
faridulhassan <faridul940@gmail.com>
2022-05-15 13:04:31 +0600

    rendered LaunchCard lists

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,8 @@
     import { Router, Route } from "svelte-routing";
     import Nav from "./components/Nav.svelte";
     import CompanyInfo from "./routes/CompanyInfo.svelte";
-    import LaunchDetails from "./routes/LaunchDetails.svelte";
     import Launches from "./routes/Launches.svelte";
     import Launchpads from "./routes/Launchpads.svelte";
-    import LaunchpadDetails from "./routes/LaunchpadDetails.svelte";
 
     export let url = "";
 </script>
@@ -13,12 +11,10 @@
 <Router {url}>
     <div class="app-container">
         <Nav />
-        <main class="container mx-auto px-4 pt-3 sm:pt-12 pb-3">
+        <main class="container mx-auto px-4 pt-3 sm:pt-12 sm:pb-7 pb-3">
             <Router>
                 <Route path="launchpads" component={Launchpads} />
-                <Route path="launchpads/:id" component={LaunchpadDetails} />
                 <Route path="launches" component={Launches} />
-                <Route path="launches/:id" component={LaunchDetails} />
                 <Route path="/" component={CompanyInfo} />
             </Router>
         </main>
